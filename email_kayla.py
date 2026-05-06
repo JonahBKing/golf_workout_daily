d9986e97e6f746a5468c0abaa22b967bb28bf4a2
Jonah King <Jonahking10@gmail.com>
2026-05-06 11:20:18 -0500

    Update email_kayla.py

--- email_kayla.py
+++ email_kayla.py
@@ -16,7 +16,7 @@ def get_day():
 
 day = get_day()
 
-msg = MIMEText("Hi Kayla, I think I found a use for chatgpt")
+msg = MIMEText("Good Morning")
 msg["Subject"] = f"HI Kayla - Test {day}"
 msg["From"] = EMAIL
 msg["To"] = RECIPIENT
